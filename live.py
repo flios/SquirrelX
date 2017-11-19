f06e4e6eb99c3e2ca0c99e636cd03b9681d31135
flios <yantengxiao@gmail.com>
2017-11-19 15:06:33 -0600

    Add a function get bitcoin price and compare the price change with the nowcasting result of modeling

--- live.py
+++ live.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import datetime
 import stream_listener
-import Queue
+import queue
 import tweepy
 import time
 import sklearn
@@ -10,6 +10,10 @@ from sklearn.externals import joblib
 import sentiment
 import features
 import json
+from  get_data import get_bitcoin_price_from_dict, get_bitcoin_price_from_coindesk
+import requests
+
+
 class TweetDataProcess:
     def __init__(self):
         self.tweet_df = pd.DataFrame()
@@ -20,19 +24,23 @@ class TweetDataProcess:
             self.tweet_df.append(pd.read_json(buff_queue.get()).loc['created_at', ['created_at', 'text']])
 
 
-
 if __name__ == '__main__':
     import twitter_setup
-    buff_que = Queue.Queue()
-    clf = joblib.load('rf.pkl')
+    buff_que = queue.Queue()
+    clf = joblib.load('rf_average_count_realtime_256_7229.pkl')
     my_api = twitter_setup.twitter_setup()
     my_listener = stream_listener.SquirrelStreamListener(buff_que)
     my_stream = tweepy.Stream(auth=my_api.auth, listener=my_listener)
     my_stream.filter(track=['bitcoin', 'btc'], languages=['en'], async=True)
     # my_listener.data_writer.running(100)
+    clint = requests.session()
+    tweet_num = 200
+    btc_price = 0.
+    last_btc_price, btw_timestamp = get_bitcoin_price_from_dict(clint.get('https://api.coindesk.com/v1/bpi/currentprice.json').json())
     while True:
-        if buff_que.qsize() > 20:
-            tweet_num = buff_que.qsize()
+        time.sleep(1)
+        if buff_que.qsize() > tweet_num:
+            # tweet_buff_size = buff_que.qsize()
             key_list = ['created_at', 'text', 'tweets']
             buff_dict = {k: [] for k in key_list}
             for i in range(tweet_num):
@@ -42,11 +50,21 @@ if __name__ == '__main__':
                     buff_dict[k].append(data_dict[k])
             tweet_df = pd.DataFrame({k: buff_dict[k] for k in buff_dict if len(buff_dict[k]) > 0})
             sentiment_df = sentiment.get_sentiment_from_tweets(tweet_df)
-            feature_list = []
-            single_feature = features.get_hist_features(sentiment_df)
-            feature_list.append(list(single_feature))
-            print clf.predict(feature_list)
-            time.sleep(10)
+            # feature_list = []
+            single_feature = features.get_average_count_features(sentiment_df)
+            # feature_list.append(list(single_feature))
+
+            btc_price, btw_timestamp = get_bitcoin_price_from_dict(clint.get('https://api.coindesk.com/v1/bpi/currentprice.json').json())
+
+            print(btw_timestamp, end=' ')
+            print('Nowcasting result:', 'up  'if(clf.predict([single_feature]) > 0) else 'down', end=' ')
+            print('            Actual price movement: ', end=' ')
+            if btc_price - last_btc_price > 0:
+                print('+',end=' ')
+
+
+            print(btc_price - last_btc_price)
+            last_btc_price = btc_price
 
     my_stream.disconnect()
     # my_listener.data_writer.stop()
